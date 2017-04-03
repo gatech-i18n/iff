d4153cc30c752773f474252b7718d1b6378d56ca
Jianing Yang <jed970610@gmail.com>
2017-04-03 14:49:13 -0400

    initial commit

---
+++ app/src/androidTest/java/edu/gatech/i18n/iff/ApplicationTest.java
@@ -0,0 +1,14 @@
+package edu.gatech.i18n.iff;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing
+ * Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
